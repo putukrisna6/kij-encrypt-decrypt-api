4cf59ebbc856498a85a948d201a1541b1590dd7b
Daniel Sugianto <danielsugianto9@gmail.com>
2022-09-30 23:17:49 +0700

    test: add decrypted cipher text equals plain text

--- src/test.cpp
+++ src/test.cpp
@@ -35,8 +35,44 @@ string generatePeriodicPlainText(int length) {
     return result;
 }
 
+// Test naming convention: https://methodpoet.com/unit-test-method-naming-convention/
+// TODO: tidy this code, maybe use testing library?
+class TestSuite {
+public:
+    void Encryption_WhenCipherTextIsDecrypted_ResultEqualsToPlainText(Encryption *encryption) {
+        vector<int> plainTextLengths;
+        plainTextLengths.push_back(32);
+        plainTextLengths.push_back(64);
+        plainTextLengths.push_back(97);
+        plainTextLengths.push_back(128);
+        plainTextLengths.push_back(256);
+        plainTextLengths.push_back(512);
+        plainTextLengths.push_back(1024);
+
+        vector<string> plainTexts;
+
+        for(int i = 0; i < plainTextLengths.size(); i++) {
+            plainTexts.push_back(generateRandomPlainText(plainTextLengths[i]));
+            plainTexts.push_back(generatePeriodicPlainText(plainTextLengths[i]));
+        }
+
+        for(int i = 0; i < plainTexts.size(); i++) {
+            string cipherText = encryption->encrypt(plainTexts[i]);
+            string decryptedCipherText = encryption->decrypt(cipherText);
+            bool isPassed = plainTexts[i] == decryptedCipherText;
+            string status = isPassed ? "pass": "fail";
+            cout << "plain text #" << i << " (length: " << plainTexts[i].size() << "): " << status << endl;
+        }
+    }
+};
+
 int main() {
     srand(time(0));
-    cout << generatePeriodicPlainText(100) << endl;
+    string key = "8_chars_";
+    TestSuite testSuite;
+
+    testSuite.Encryption_WhenCipherTextIsDecrypted_ResultEqualsToPlainText(new ARC4(key));
+    testSuite.Encryption_WhenCipherTextIsDecrypted_ResultEqualsToPlainText(new DES(key));
+
     return 0;
 }
