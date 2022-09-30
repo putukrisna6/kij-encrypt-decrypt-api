9f534c8df86ce7655cb17284ed4278691ac815e5
Daniel Sugianto <danielsugianto9@gmail.com>
2022-09-30 22:37:09 +0700

    feat: add periodic plain text generator

--- src/test.cpp
+++ src/test.cpp
@@ -4,6 +4,9 @@
 #include "algorithms/des.h"
 using namespace std;
 
+const int minPrintableAsciiCharacter = 32;
+const int maxPrintableAsciiCharacter = 127;
+
 // Interval: [startNumber, endNumber]
 int generateRandomNumber(int startNumber, int endNumber) {
     return startNumber + rand() % (endNumber - startNumber + 1);
@@ -11,7 +14,7 @@ int generateRandomNumber(int startNumber, int endNumber) {
 
 // Character code interval: [32, 127]
 char generateRandomPrintableAsciiCharacter() {
-    char c = (char) generateRandomNumber(32, 127);
+    char c = (char) generateRandomNumber(minPrintableAsciiCharacter, maxPrintableAsciiCharacter);
     return c;
 }
 
@@ -23,11 +26,17 @@ string generateRandomPlainText(int length) {
     return result;
 }
 
+string generatePeriodicPlainText(int length) {
+    string result;
+    for(int i = 0; i < length; i++) {
+        char c = (char) (minPrintableAsciiCharacter + i % (minPrintableAsciiCharacter - maxPrintableAsciiCharacter + 1));
+        result += c;
+    }
+    return result;
+}
+
 int main() {
     srand(time(0));
-    for(int i = 0; i < 10; i++) {
-        cout << generateRandomPlainText(4) << endl;
-    }
-
+    cout << generatePeriodicPlainText(100) << endl;
     return 0;
 }
