c7fd6fd992d53c6d2e988ee47b3fec123a26d4b0
Daniel Sugianto <danielsugianto9@gmail.com>
2022-10-01 09:29:41 +0700

    feat: add measure algorithm average run time
    - move generators from test.cpp to generator.h

---
+++ src/algorithms/helpers/generator.h
@@ -0,0 +1,38 @@
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int minPrintableAsciiCharacter = 32;
+const int maxPrintableAsciiCharacter = 127;
+
+// Interval: [startNumber, endNumber]
+int generateRandomNumber(int startNumber, int endNumber) {
+    return startNumber + rand() % (endNumber - startNumber + 1);
+}
+
+// Character code interval: [32, 127]
+char generateRandomPrintableAsciiCharacter() {
+    char c = (char) generateRandomNumber(minPrintableAsciiCharacter, maxPrintableAsciiCharacter);
+    return c;
+}
+
+string generateRandomPlainText(int length) {
+    string result;
+    for(int i = 0; i < length; i++) {
+        result += generateRandomPrintableAsciiCharacter();
+    }
+    return result;
+}
+
+string generatePeriodicPlainText(int length) {
+    string result;
+    for(int i = 0; i < length; i++) {
+        char c = (char) (minPrintableAsciiCharacter + i % (minPrintableAsciiCharacter - maxPrintableAsciiCharacter + 1));
+        result += c;
+    }
+    return result;
+}
+
+#endif
